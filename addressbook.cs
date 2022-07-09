e18ec590c676297aedc7467ad2a6e18653d4e3fd
unknown <abhisheksrivastava9719@gmail.com>
2022-07-09 06:55:48 +0530

    [Abhishek]Added the editing contact functionality

--- addressbook.cs
+++ addressbook.cs
@@ -8,7 +8,7 @@ namespace Addressbook
 {
     public class addressBook
     {
-         List<contact> add_book = new List<contact>();          //created an address book list of contact class type
+        List<contact> add_book = new List<contact>();          //created an address book list of contact class type
         public void create()
         {
             contact Contact = new contact();                    //created an object of the Contact class and added contact details
@@ -23,25 +23,55 @@ namespace Addressbook
             Console.WriteLine("Enter your state");
             Contact.state = Console.ReadLine();
             Console.WriteLine("Enter your zip code");
-            Contact.zip =Convert.ToInt64(Console.ReadLine());
+            Contact.zip = Convert.ToInt64(Console.ReadLine());
             Console.WriteLine("Enter your phone number");
-            Contact.phone_no= Convert.ToInt64(Console.ReadLine());
+            Contact.phone_no = Convert.ToInt64(Console.ReadLine());
             Console.WriteLine("Enter your email");
             Contact.email = Console.ReadLine();
             add_book.Add(Contact);
+            Console.WriteLine();
+        }
+        public void edit(String Fname, String Sname) //Editing a contact
+        {
+            contact Contact = new contact();
+            foreach (var person in add_book)
+            {
+                if (person.first_name.Equals(Fname) && person.s_name.Equals(Sname))
+                {
+                    Console.WriteLine("Edit and confirm the details now -");
+                    Console.WriteLine("Enter your first name");
+                    person.first_name = Console.ReadLine();
+                    Console.WriteLine("Enter your second name");
+                    person.s_name = Console.ReadLine();
+                    Console.WriteLine("Enter your address");
+                    person.address = Console.ReadLine();
+                    Console.WriteLine("Enter your city");
+                    person.city = Console.ReadLine();
+                    Console.WriteLine("Enter your state");
+                    person.state = Console.ReadLine();
+                    Console.WriteLine("Enter your zip code");
+                    person.zip = Convert.ToInt64(Console.ReadLine());
+                    Console.WriteLine("Enter your phone number");
+                    person.phone_no = Convert.ToInt64(Console.ReadLine());
+                    Console.WriteLine("Enter your email");
+                    person.email = Console.ReadLine();
+                    Console.WriteLine();
+                }
+            }
         }
         public void display()               //displaying details of each conatact
         {
-            foreach(var data in add_book )
+            foreach (var data in add_book)
             {
-                Console.WriteLine("Contact No. - "+data);
-                Console.WriteLine("Name - "+data.first_name+" "+data.s_name);
+                Console.WriteLine("Contact Name. - " + data.first_name);
+                Console.WriteLine("Full Name - " + data.first_name + " " + data.s_name);
                 Console.WriteLine("Address - " + data.address);
                 Console.WriteLine("City- " + data.city);
                 Console.WriteLine("State - " + data.state);
                 Console.WriteLine("Zip - " + data.zip);
                 Console.WriteLine("Phone no. - " + data.phone_no);
                 Console.WriteLine("Email - " + data.email);
+                Console.WriteLine();
             }
         }
     }
